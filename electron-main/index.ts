d13ab18dd01f21fe19f54a0426758e01f63f2ab3
bobingo <17642059382@163.com>
2022-06-10 19:01:13 +0800

    electron-main/index.ts中增加判断系统的逻辑

--- electron-main/index.ts
+++ electron-main/index.ts
@@ -1,29 +1,31 @@
 import { app, BrowserWindow, screen } from 'electron';
 import is_dev from 'electron-is-dev';
 import { join } from 'path';
-// import net from 'net';
-// import child_process from 'child_process';
+// import net from 'net'; // 使用 from 方式引入 会 报错，没找到原因
+// import child_process from 'child_process'; // 使用 from 方式引入 会 报错，没找到原因
 const net = require('net');
 const child_process = require('child_process');
 // 解决“Electron Security Warning”安全警告: https://www.chfse.com/archives/2495
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 let mainWindow: BrowserWindow | null = null;
-const exePath = `${join(__dirname, '../PIPServe.exe')}`;
 
-console.log(1111, exePath);
-const cmdStrServer = `start ${exePath}`;
-
-function runExec() {
-  // 使用 spawn 运行 ./PIPServe.exe，spawn运行的子进程会在主进程关闭时一起关闭
+function runExec(exePath, _cmdStrServer) {
+  // 使用 spawn 运行 PIPServe.exe，spawn运行的子进程会在主进程关闭时一起关闭
   child_process.spawn(exePath, [], {
     windowsHide: false, // 隐藏子进程的窗口
   });
-  //// 使用 exec 运行 ./PIPServe.exe， exec 运行的子进程不会在主进程关闭时一起关闭
-  // child_process.exec(cmdStrServer, {});
-  // child_process.exec(cmdStrClient, {})
+  //// 使用 exec 运行 PIPServe.exe， exec 运行的子进程不会在主进程关闭时一起关闭
+  // child_process.exec(_cmdStrServer, {});  // 服务端，需要启动
+  //// child_process.exec(_cmdStrClient, {}) // 客户端，不需要启动
 }
 
-runExec(); // 调用子进程
+if (process.platform === 'win32') {
+  // 这是windows系统, mac系统下调用不了.exe文件
+  //TODO PIPServe.exe 这个文件放到public中，打包后会被打到dist中，dist文件夹中的静态资源的路径就是下面的 exePath
+  const exePath = `${join(__dirname, '../PIPServe.exe')}`;
+  const _cmdStrServer = `start ${exePath}`;
+  runExec(exePath, _cmdStrServer); // 调用子进程
+}
 
 const connectserver = () => {
   // node通过创建命名管道与 c 或者 julia 的 .exe文件通信
@@ -81,7 +83,10 @@ class createWin {
     // if (is_dev) {
     mainWindow.webContents.openDevTools();
     // }
-    connectserver(); // 与子进程通信
+    if (process.platform === 'win32') {
+      connectserver(); // 与子进程通信
+    }
+
     mainWindow.on('ready-to-show', () => {
       mainWindow.show();
     });
